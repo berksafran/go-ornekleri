c314dc74413c7b9a56ff7ce57694d2fe84e28d48
Berk Safran <berksafranbolulu@gmail.com>
2021-03-22 01:44:47 +0300

    Move all files to new repository.

--- go-url-shortener/handlers/mainHandler.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "github.com/labstack/echo/v4"
-
-// MainHandler is handler of "/"
-func MainHandler(c echo.Context) error {
-	return c.String(200, "Hello World. \nThis is Shorty.")
-}
